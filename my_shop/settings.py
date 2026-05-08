955eb97e940c5cb902bc3ef69566f24323638284
beligwsr <uvybelig@gmail.com>
2026-05-08 12:31:57 +0800

    Update settings.py

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-your-secret-key-change-this-123456'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
